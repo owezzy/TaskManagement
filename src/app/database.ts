01a446d6a2cdddb39060daa8312d24a7d0faaf76
owezzy <owenobezzy@gmail.com>
2019-06-08 14:09:07 +0300

    projects data interface and model

--- src/app/database.ts
+++ src/app/database.ts
@@ -1,16 +1,21 @@
 import {InMemoryDbService} from 'angular-in-memory-web-api';
-import {Task} from './model';
+import {Project, Task} from './model';
 
 
 export class Database implements InMemoryDbService {
 
   createDb() {
-    const tasks: Task[] = [
-      {id: 1, title: 'Task 1', done: false},
-      {id: 2, title: 'Task 2', done: true},
-      {id: 3, title: 'Task 3', done: false},
-      {id: 4, title: 'Task 4', done: true}
+    const projects: Project[] = [
+      {id: 1, title: 'First Project', description: 'Project Uno'},
+      {id: 2, title: 'Second Project', description: 'Project Duex'},
     ];
-    return {tasks};
+
+    const tasks: Task[] = [
+      {id: 1, projectId: 1, title: 'Task 1', done: false},
+      {id: 2, projectId: 2, title: 'Task 2', done: true},
+      {id: 3, projectId: 1, title: 'Task 3', done: false},
+      {id: 4, projectId: 2, title: 'Task 4', done: true}
+    ];
+    return {projects, tasks};
   }
 }
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,7 +1,15 @@
 export interface Task {
   readonly id?: number;
+  readonly projectId?: number;
   readonly title: string;
   readonly done: boolean;
 }
 
 export type TaskListFilterType = 'all' | 'open' | 'done';
+
+
+export interface Project {
+  readonly id?: number;
+  readonly title: string;
+  readonly  description: string;
+}
