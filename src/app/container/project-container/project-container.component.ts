3a844a0b7392d58a2291e1376adcf287c47b3147
owezzy <owenobezzy@gmail.com>
2019-06-10 08:05:54 +0300

    change activeTab to a stream data type and refactor component to use route params

--- src/app/container/project-container/project-container.component.ts
+++ src/app/container/project-container/project-container.component.ts
@@ -1,7 +1,9 @@
 import {Component, ViewEncapsulation, ChangeDetectionStrategy} from '@angular/core';
-import {Observable} from 'rxjs';
+import {combineLatest, Observable} from 'rxjs';
 import {Project, Tab} from '../../model';
 import {ProjectService} from '../../project/project.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-container',
@@ -18,14 +20,37 @@ export class ProjectContainerComponent {
     {id: 'comments', title: 'Comments'},
     {id: 'activities', title: 'Activities'}
   ];
-  activeTab: Tab = this.tabs[0];
+  activeTab: Observable<Tab>;
 
-  constructor(private projectService: ProjectService) {
-    this.selectedProject = projectService.getSelectedProject();
+  constructor(
+    private projectService: ProjectService,
+    private route: ActivatedRoute,
+    private router: Router) {
+
+    this.selectedProject = combineLatest(
+      projectService.getProjects(),
+      route.params)
+      .pipe(
+        map(([projects, routeParams]) =>
+          projects.find((project) => project.id === +routeParams.projectId))
+      );
+
+    this.activeTab = combineLatest(
+      this.selectedProject, route.url
+    ).pipe(
+      map(([project]) => this.tabs.find((tab) =>
+        router.isActive(
+          `/projects/${project.id}/${tab.id}`, false
+        )))
+    );
   }
 
   activateTab(tab: Tab) {
-    this.activeTab = tab;
+    this.selectedProject
+      .pipe(take(1))
+      .subscribe((project: Project) => {
+        this.router.navigate(['/projects', project.id, tab.id]);
+      });
   }
 
   updateProject(project: Project) {
